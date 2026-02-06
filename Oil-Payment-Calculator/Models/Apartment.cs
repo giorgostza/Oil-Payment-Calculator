64cc474e6a2b422a061f3e5cd464192748aa1138
ΓΙΩΡΓΟΣ ΤΖΑΒΕΛΛΑΣ <giorgostza91@gmail.com>
2026-02-06 14:41:30 +0200

    create Apartment and MeterReading Model

---
+++ Oil-Payment-Calculator/Models/Apartment.cs
@@ -0,0 +1,22 @@
+﻿namespace Oil_Payment_Calculator.Models
+{
+
+    public enum ApartmentType
+    {
+        Floor,        // Οροφος
+        Shop,         // Καταστημα-ισογειο
+        Basement      // Υπογειο
+    }
+
+    public class Apartment
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ApartmentType Type { get; set; }
+
+
+    }
+}
---
+++ Oil-Payment-Calculator/Models/MeterReading.cs
@@ -0,0 +1,24 @@
+﻿namespace Oil_Payment_Calculator.Models
+{
+    public class MeterReading
+    {
+
+        public int Id { get; set; }
+
+        public int ApartmentId { get; set; }
+
+        public Apartment Apartment { get; set; }
+
+        public int PreviousReading { get; set; }
+
+        public int CurrentReading { get; set; }
+
+
+        public int GetDifference()
+        {
+            return CurrentReading - PreviousReading;
+        }
+
+
+    }
+}
