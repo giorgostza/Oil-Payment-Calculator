d837cc5b405620ec49ca597e74a85e0cd80a31c1
ΓΙΩΡΓΟΣ ΤΖΑΒΕΛΛΑΣ <giorgostza91@gmail.com>
2026-02-06 12:54:00 +0200

    Add project files.

---
+++ Oil-Payment-Calculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oil_Payment_Calculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
